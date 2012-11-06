71b67aae66d93d0a4da59a933449c6e801470734
soarpenguin <soarpenguin@gmail.com>
2012-11-06 21:12:18 +0800

    add a client code connect echo server.

--- socket.pl
+++ socket.pl
@@ -1,13 +1,25 @@
 #!/usr/bin/perl -w
 #
+use Socket qw(PF_INET SOCK_STREAM pack_sockaddr_in inet_aton);
 
-use IO::Socket;
-my $sock = new IO::Socket::INET (
-	 LocalHost => 'thekla',
-	 LocalPort => '7070', 
-	 Proto => 'tcp', 
-	 Listen => 1, 
-	 Reuse => 1, 
-); 
+socket(my $socket, PF_INET, SOCK_STREAM, 0)
+	or die "socket: $!";
 
-die "Could not create socket: $!\n" unless $sock;
+my $port = getservbyname "echo", "tcp";
+connect($socket, pack_sockaddr_in($port, inet_aton("localhost")))
+	or die "connect: $!";
+
+print $socket "Hello, world!\n";
+
+print <$socket>;
+
+#use IO::Socket;
+#my $sock = new IO::Socket::INET (
+#	 LocalHost => 'thekla',
+#	 LocalPort => '7070', 
+#	 Proto => 'tcp', 
+#	 Listen => 1, 
+#	 Reuse => 1, 
+#); 
+#
+#die "Could not create socket: $!\n" unless $sock;
