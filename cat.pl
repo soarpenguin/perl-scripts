ae49089140e7a1b444693fc5262bc084ce603f48
soarpenguin <soarpenguin@gmail.com>
2012-11-13 16:15:43 +0800

    cat.pl is simple command of 'cat'.

---
+++ cat.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Basename;
+
+my $index = 0;
+my $linenum = 1;
+my $script = basename $0;
+my $debug = 0;
+my ( $number, $tab, $end, $help, $version );
+
+my $Usage = " 
+Usage: $script [option]... [file]... 
+       -n, --[no]number
+              number all output lines or disable with [nonumber]
+
+       -E, --show-ends
+              display \$ at end of each line
+
+       -T, --show-tabs
+              display TAB characters as ^I
+
+       -h, --help 
+			  display this help and exit
+
+       --version
+              output version information and exit
+";
+
+GetOptions(
+	'number|n!' => \$number,  #the '!' means can use --[no]number disable -n option
+	'show-ends|E' => \$end,
+	'show-tabs|T' => \$tab,
+	'help|h'	=> \$help,
+	'version|v' => \$version,
+	'debug|d'	=> \$debug  # use for debug, turn on Smart::Comments;
+);
+
+if($debug) {
+	# use Smart::Comments;
+}
+
+my @files = @ARGV;
+
+if($help or $version) {
+	&usage();
+}
+
+while(my $line = <>)
+{
+	if($number) {
+		printf ("%6d  ", $linenum++);
+		if($tab) {
+			$line =~ s/\t/\^I/sg;
+			if($end) {
+				$line =~ s/(\n|\n\r)/\$$1/;
+			}
+		}
+		print $line;
+		if(eof) {
+			print("--------end of $files[$index] file--------\n");
+			$index += 1;
+		}
+	} else {
+		if($tab) {
+			$line =~ s/\t/\^I/sg;
+			if($end) {
+				$line =~ s/(\n|\n\r)/\$$1/;
+			}
+		}
+		print $line;
+		if(eof) {
+			print("--------end of $files[$index] file--------\n");
+			$index += 1;
+		}
+	}
+}
+
+sub usage {
+	print $Usage;
+	exit;
+}
+
+### @files
+### $index
+### $tab
+### @ARGV
+### $number
+### $help
+### $debug
+
