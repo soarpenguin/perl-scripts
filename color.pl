623ff6dcdad4e595da65a9f0deaf6bcf466e8c9c
soarpenguin <soarpenguin@gmail.com>
2013-03-09 21:07:29 +0800

    add ANSI Escapr sequences: colours and cursor movement usage.

--- color.pl
+++ color.pl
@@ -47,6 +47,44 @@ print "This text is red on green.\n";
 }
 print POPCOLOR "Back to whatever we started as.\n";
 
+#\033[1;34m     or     \e[1;34m
+#Black       	0;30     Dark Gray     	1;30
+#Blue        	0;34     Light Blue    	1;34
+#Green       	0;32     Light Green   	1;32
+#Cyan        	0;36     Light Cyan    	1;36
+#Red         	0;31     Light Red     	1;31
+#Purple      	0;35     Light Purple  	1;35
+#Brown       	0;33     Yellow        	1;33
+#Light Gray  	0;37     White         	1;37
+#
+# -Ends the colour.
+#  \033[0m
+#  \x1b[0m
+#
+#- Position the Cursor:
+#  \033[<L>;<C>H
+#     Or
+#  \033[<L>;<C>f
+#  puts the cursor at line L and column C.
+#- Move the cursor up N lines:
+#  \033[<N>A
+#- Move the cursor down N lines:
+#  \033[<N>B
+#- Move the cursor forward N columns:
+#  \033[<N>C
+#- Move the cursor backward N columns:
+#  \033[<N>D
+#
+#- Clear the screen, move to (0,0):
+#  \033[2J
+#- Erase to end of line:
+#  \033[K
+#
+#- Save cursor position:
+#  \033[s
+#- Restore cursor position:
+#  \033[u
+
 ##################################################################
 #!/usr/bin/perl
 # Author: Todd Larason <jtl@molehill.org>
