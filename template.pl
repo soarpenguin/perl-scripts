5c49bde1462ace3290e577f20030b32647580e84
soarpenguin <soarpenguin@gmail.com>
2013-08-11 00:12:59 +0800

    add a template perl script: template.pl

---
+++ template.pl
@@ -0,0 +1,13 @@
+#!/bin/env perl
+#
+
+BEGIN {
+    use Cwd 'realpath';
+    our $curdir;
+    $curdir = __FILE__;
+    $curdir = realpath($curdir);
+    $curdir =~ s/[^\/]+$//;
+    ### $curdir
+    #push @INC, "$curdir/lib/";
+}
+
