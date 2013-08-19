387d69dc430d7969a3ef1d5da0759c1f478610f6
soarpenguin <soarpenguin@gmail.com>
2013-08-20 00:21:53 +0800

    add a batch send file or run command script.

---
+++ batch_send.pl
@@ -0,0 +1,41 @@
+#!/bin/env perl
+#
+
+BEGIN {
+    use Cwd 'realpath';
+    our $curdir;
+    $curdir = __FILE__;
+    $curdir = realpath($curdir);
+    $curdir =~ s/[^\/]+$//;
+    ### $curdir
+    #push @INC, "$curdir/lib/";
+}
+
+use strict;
+use warnings;
+
+my $script = &my_program();
+
+&main(@ARGV);
+
+sub main {
+    print &usage();
+}
+
+sub usage {
+    return <<EOT
+Usage: $script [option]... -s src -d dst hostlist
+    -s : Source file for distribution.
+
+    -d : Destination dir or file for distribution.
+
+    -p : Set parallel for distribution.
+
+    -c : Run the command for hostlist.
+EOT
+}
+
+sub my_program {
+    require File::Basename;
+    return File::Basename::basename( $0 );
+} 
