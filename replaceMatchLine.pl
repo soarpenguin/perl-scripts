b34e5dd4e054600a8f48daedcf0417c1258357d8
soarpenguin <soarpenguin@gmail.com>
2013-09-24 22:58:23 +0800

    replace match line one 2 one, modify the destfile.

---
+++ replaceMatchLine.pl
@@ -0,0 +1,52 @@
+#!/bin/env perl
+
+
+if (@ARGV != 3) {
+    print "perl $0 origfile replfile destfile.\n";
+    exit 1;
+}
+
+my $original = $ARGV[0];
+my $replace = $ARGV[1];
+my $destfile = $ARGV[2];
+
+open(OFH, "<", $original) or die "open $original file failed.";
+open(RFH, "<",  $replace) or die "open $original file failed.";
+
+my @orig = <OFH>;
+my @rep = <RFH>;
+
+close(OFH);
+close(RFH);
+
+if (scalar @orig != scalar @rep) {
+    print "The file of $original and $replace line num not matched.\n";
+    exit 1;
+}
+
+my $count = 0;
+
+foreach (@orig) {
+
+    chomp($orig[$count]);
+    chomp($rep[$count]);
+    
+    &replace($orig[$count], $rep[$count], $destfile);
+
+    $count++;
+}
+
+
+sub replace {
+    my $orig = shift;
+    my $rep = shift;
+    my $dest = shift;
+
+
+    `fgrep "${orig}" ${dest} &>/dev/null`;
+    if($? eq 0) {
+        `sed -e "s/${orig}/${rep}/" ${dest} >${dest}.tmp`;
+        `mv ${dest}.tmp ${dest}`;
+    }
+}
+           
