9efbcdbf4b24856a96fcf98ed8831e3d40c50909
soarpenguin <soarpenguin@gmail.com>
2013-11-01 00:09:31 +0800

    a bash script for thread running.

---
+++ bashrc/threads.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+g_HOST_LIST=$1
+g_THREAD_NUM=300
+tmp_file="pipe.$$"
+SSH="ssh -n -o PasswordAuthentication=no -o StrictHostKeyChecking=no -o ConnectTimeout=5 "
+SCP='scp -q -r -o PasswordAuthentication=no -o StrictHostKeyChecking=no -o ConnectTimeout=5 '
+
+if [ -d log ]; then
+    rm -rf log
+fi
+
+mkdir -p log
+
+mkfifo ${tmp_file}
+exec 9<>${tmp_file}
+
+for ((i=0;i<${g_THREAD_NUM};i++))
+do
+    echo >&9
+done
+
+unset HOST
+for HOST in `cat ${g_HOST_LIST}`
+do
+    echo "start  ${HOST} ......"
+    read <&9
+
+    (${SSH} ${HOST} "ls "  &>  log/${HOST}; echo >&9) &
+
+done
+rm -f ${tmp_file}
+exec 9<&-
