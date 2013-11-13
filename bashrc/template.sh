5f934103331e641c829623d8b99a6b69f17aa041
soarpenguin <soarpenguin@gmail.com>
2013-11-14 00:37:21 +0800

    Update template.sh

--- bashrc/template.sh
+++ bashrc/template.sh
@@ -3,5 +3,11 @@
 #export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }';
 #curdir=$(cd `dirname $0`; pwd);
 
-export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
-curdir=$(cd "$(dirname "$0")"; pwd)
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }';
+curdir=$(cd "$(dirname "$0")"; pwd);
+curdir=$(dirname $(readlink -f "$0"));
+
+#tar -cf - ./* | ( cd "${dir}" && tar -xf - )
+#if [[ "${PIPESTATUS[0]}" -ne 0 || "${PIPESTATUS[1]}" -ne 0 ]]; then
+#  echo "Unable to tar files to ${dir}" >&2
+#fi
