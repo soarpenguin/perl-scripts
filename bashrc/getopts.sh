f1af38dce95d13e6981a97ccbd78cf975c06f426
soarpenguin <soarpenguin@gmail.com>
2013-12-13 01:06:27 +0800

    Update getopts.sh

--- bashrc/getopts.sh
+++ bashrc/getopts.sh
@@ -16,4 +16,20 @@ while getopts "cn" opt; do
     esac
 done
 
+while getopts ":a:" opt; do
+  case $opt in
+    a)
+      echo "-a was triggered, Parameter: $OPTARG" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 echo "The type is $Type";
