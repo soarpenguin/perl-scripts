4421b383572b00d3da6e298b2e86514cca06503e
soarpenguin <soarpenguin@gmail.com>
2014-03-07 00:22:56 +0800

    python daemonize usage example.

---
+++ bashrc/daemonize.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import errno
+import fcntl
+import os
+import random
+import re
+import signal
+import socket
+import sys
+import threading
+import time
+
+
+def daemonize():
+    """Performs the necessary dance to become a background daemon."""
+    if os.fork():
+        os._exit(0)
+    os.chdir("/")
+    os.umask(022)
+    os.setsid()
+    os.umask(0)
+    if os.fork():
+        os._exit(0)
+    stdin = open(os.devnull)
+    stdout = open(os.devnull, 'w')
+    os.dup2(stdin.fileno(), 0)
+    os.dup2(stdout.fileno(), 1)
+    os.dup2(stdout.fileno(), 2)
+    stdin.close()
+    stdout.close()
+    for fd in xrange(3, 1024):
+        try:
+            os.close(fd)
+        except OSError:  # This FD wasn't opened...
+            pass         # ... ignore the exception.
+
