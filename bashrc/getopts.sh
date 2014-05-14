b4aafd6f27e855517ea8fcef00e4d2791aa342ac
soarpenguin <soarpenguin@gmail.com>
2014-05-14 14:27:29 +0800

    Update getopts.sh

--- bashrc/getopts.sh
+++ bashrc/getopts.sh
@@ -33,3 +33,48 @@ while getopts ":a:" opt; do
 done
 
 echo "The type is $Type";
+
+function usage
+{
+        echo "bash $0 --name=NAME --workspace=WORKSPACE --base=BASE --level=LEVEL --table=TABLE [--help|-h]"
+}
+
+topdir=`dirname $0`;
+ARGS=`getopt -a -o w:b:l:t:h -l name:,workspace:,base:,level:,table:,help -- "$@"`
+[ $? -ne 0 ] && usage
+eval set -- "${ARGS}"
+
+while true  
+do
+        case "$1" in
+        --name)
+                name="$2"
+                shift 2
+                ;;
+        --workspace)
+                workspace="${2}"
+                shift 2
+                ;;
+        --base)
+                base="$2"
+                shift 2
+                ;;
+        --level)
+                level="$2"
+                shift 2
+                ;;
+        --table)
+                table="$2"
+                shift 2
+                ;;
+        --help|-h)
+                echo "unknown arg: $1"
+                usage
+                exit 1
+                ;;
+        --)
+                shift
+                break
+                ;;
+        esac
+done
