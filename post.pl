5c9de4c7b978b71f380cc3e5ec0ab7d3dac9bc51
soarpenguin <soarpenguin@gmail.com>
2014-07-21 19:51:43 +0800

    add MD5 and LWP usage.

--- post.pl
+++ post.pl
@@ -33,8 +33,30 @@ print Dumper($ua);
 
 my $resp = $ua->request($req);
 if ($resp->is_success) {
-		print Dumper($resp->content);
+	print Dumper($resp->content);
 } else {
-		print "HTTP POST error code: ", $resp->code, "\n";
-		print "HTTP POST error message: ", $resp->message, "\n";
+	print "HTTP POST error code: ", $resp->code, "\n";
+	print "HTTP POST error message: ", $resp->message, "\n";
+}
+
+my $timestamp = int(time());
+my $md5string = "$timestamp";
+my $md5 = Digest::MD5->new;
+$md5->add($md5string);
+my $accessToken = $md5->hexdigest;
+my $ua=LWP::UserAgent->new;
+$ua->timeout(100);
+
+my $req = $ua->post($url,
+  [
+       "timeStamp"   => "$timestamp"
+  ]
+);
+
+if ($req->is_success) {
+       my $content = $req->content();
+       print "$content\n";
+} else {
+       print $req->status_line, "\n";
+       print $req->message;
 }
