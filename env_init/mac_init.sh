c2436fa482071a8b2c63851ed0bffba54a160127
soarpenguin <soarpenguin@gmail.com>
2014-10-08 23:36:52 +0800

    mac_init.sh add some software for install

--- env_init/mac_init.sh
+++ env_init/mac_init.sh
@@ -32,4 +32,13 @@ fi
 
 # Update homebrew recipes
 brew update
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+# Install Bash 4
+brew install bash
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
 
