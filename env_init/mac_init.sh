8e58ae12c8d0983762c5495497bb4df44f38f763
soarpenguin <soarpenguin@gmail.com>
2014-12-08 15:54:46 +0800

    mac_init.sh - update and improve mac_init.sh

--- env_init/mac_init.sh
+++ env_init/mac_init.sh
@@ -4,29 +4,63 @@ export PS4='$0.$LINENO+ '
 AUTO_INVOKE_SUDO=yes
 curdir=$(cd "$(dirname "$0")"; pwd)
 
+##################### function #########################
+report_err() { echo "${MYNAME}: Error: $*" >&2 ; }
+
+if [ -t 1 ]
+then
+    RED="$( echo -e "\e[31m" )"
+    HL_RED="$( echo -e "\e[31;1m" )"
+    HL_BLUE="$( echo -e "\e[34;1m" )"
+
+    NORMAL="$( echo -e "\e[0m" )"
+fi
+
+_hl_red()    { echo "$HL_RED""$@""$NORMAL";}
+_hl_blue()   { echo "$HL_BLUE""$@""$NORMAL";}
+
+_trace() {
+    echo $(_hl_blue '  ->') "$@" >&2
+}
+
+_print_fatal() {
+    echo $(_hl_red '==>') "$@" >&2
+}
+
+function lowercase() {
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
+
 function invoke_sudo() 
 {
     if [ "`id -u`" != "`id -u $1`" ]; then
         echo "`whoami`:you need to be $1 privilege to run this script.";
         if [ "$AUTO_INVOKE_SUDO" == "yes" ]; then 
-            echo "Invoking sudo ...";
+            _trace "Invoking sudo ...";
             sudo -u "#`id -u $1`" sh -c "$2";
         fi
         exit 0;
     fi
 }
 
+#################### main route ########################
+OS=`uname`
+
+if [ "$OS" != "darwin" ]; then
+    _print_fatal "Not darwin, this script just for mac env init."
+    exit 1
+fi
+
+
 uid=`id -u`
 if [ $uid -ne '0' ]; then 
-  # echo "Must running in root privilege.";
-  # exit
   invoke_sudo root "${curdir}/$0 $@"
 fi
 
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
-  echo "Installing homebrew..."
+  _trace "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
