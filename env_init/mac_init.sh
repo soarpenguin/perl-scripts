acd84c5ff8fd9a9454d4bee3ceeed590c1924582
soarpenguin <soarpenguin@gmail.com>
2015-03-06 09:22:04 +0800

    update mac_init.sh, use CONST for color.

--- env_init/mac_init.sh
+++ env_init/mac_init.sh
@@ -12,7 +12,7 @@ RETFAIL=1
 RETOK=0
 
 ##################### function #########################
-report_err() { echo "${MYNAME}: Error: $*" >&2 ; }
+_report_err() { echo "${MYNAME}: Error: $*" >&2 ; }
 
 if [ -t 1 ]
 then
@@ -62,47 +62,47 @@ _install_zsh() {
     fi
 
     if [ -d "$ZSH" ]; then
-      echo "\033[0;33mYou already have Oh My Zsh installed.\033[0m You'll need to remove $ZSH if you want to install"
+      echo "\033[0;33mYou already have Oh My Zsh installed.${NORMAL} You'll need to remove $ZSH if you want to install"
       return $RETOK
     fi
 
-    echo "\033[0;34mCloning Oh My Zsh...\033[0m"
+    echo "\033[0;34mCloning Oh My Zsh...${NORMAL}"
     hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
       echo "git not installed"
       exit $RETFAIL
     }
 
-    echo "\033[0;34mLooking for an existing zsh config...\033[0m"
+    echo "\033[0;34mLooking for an existing zsh config...${NORMAL}"
     if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
-      echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.zshrc.pre-oh-my-zsh\033[0m";
-      mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
+      echo "\033[0;33mFound ~/.zshrc.${NORMAL} \033[0;32mBacking up to ~/.zshrc.pre-oh-my-zsh${NORMAL}"
+      mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh
     fi
 
-    echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
+    echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}"
     cp $ZSH/templates/zshrc.zsh-template ~/.zshrc
     sed -i -e "/^export ZSH=/ c\\
     export ZSH=$ZSH
     " ~/.zshrc
 
-    echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
+    echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.${NORMAL}"
     sed -i -e "/export PATH=/ c\\
     export PATH=\"$PATH\"
     " ~/.zshrc
 
     if [ "$SHELL" != "$(which zsh)" ]; then
-        echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
+        echo "\033[0;34mTime to change your default shell to zsh!${NORMAL}"
         chsh -s `which zsh`
     fi
 
-    echo "\033[0;32m"'         __                                     __   '"\033[0m"
-    echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
-    echo "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"\033[0m"
-    echo "\033[0;32m"'/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '"\033[0m"
-    echo "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"\033[0m"
-    echo "\033[0;32m"'                        /____/                       ....is now installed!'"\033[0m"
-    echo "\n\n \033[0;32mPlease look over the ~/.zshrc file to select plugins, themes, and options.\033[0m"
-    echo "\n\n \033[0;32mp.s. Follow us at http://twitter.com/ohmyzsh.\033[0m"
-    echo "\n\n \033[0;32mp.p.s. Get stickers and t-shirts at http://shop.planetargon.com.\033[0m"
+    echo "\033[0;32m"'         __                                     __   '"${NORMAL}"
+    echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"${NORMAL}"
+    echo "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"${NORMAL}"
+    echo "\033[0;32m"'/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '"${NORMAL}"
+    echo "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"${NORMAL}"
+    echo "\033[0;32m"'                        /____/                       ....is now installed!'"${NORMAL}"
+    echo "\n\n \033[0;32mPlease look over the ~/.zshrc file to select plugins, themes, and options.${NORMAL}"
+    echo "\n\n \033[0;32mp.s. Follow us at http://twitter.com/ohmyzsh.{$NORMAL}"
+    echo "\n\n \033[0;32mp.p.s. Get stickers and t-shirts at http://shop.planetargon.com.${NORMAL}"
     env zsh
     . ~/.zshrc
 }
@@ -123,6 +123,7 @@ fi
 #curl -L http://install.ohmyz.sh | sh
 _install_zsh || _print_fatal "Install zsh failed!"
 
+exit
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
